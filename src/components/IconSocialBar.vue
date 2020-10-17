71844b6e1e20bc164db8b9c59b6bd59098ebeb8e
Ivan <iva.drakon.nov@gmail.com>
2020-10-17 15:23:30 +0200

    Social Icons Bar

--- src/components/IconSocialBar.vue
+++ src/components/IconSocialBar.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="iconbar">
-    <icon-base class="iconbar__image" icon-name="facebook" width="64" height="64" icon-color="#5434ad"><socialfacebook/></icon-base>
-    <icon-base class="iconbar__image" icon-name="instagram" width="64" height="64" icon-color="#5434ad"><socialinstagram/></icon-base>
+  <div class="icons">
+    <icon-base class="icons__image" icon-name="facebook" width="64" height="64" icon-color="#5434ad"><socialfacebook/></icon-base>
+    <icon-base class="icons__image" icon-name="instagram" width="64" height="64" icon-color="#5434ad"><socialinstagram/></icon-base>
     <icon-base class="icons__image" icon-name="linkedin" width="64" height="64" icon-color="#5434ad"><sociallinkedin/></icon-base>
     <icon-base class="icons__image" icon-name="odnolassniki" width="64" height="64" icon-color="#5434ad"><socialodnolassniki/></icon-base>
     <icon-base class="icons__image" icon-name="tumblr" width="64" height="64" icon-color="#5434ad"><socialtumblr/></icon-base>
@@ -11,6 +11,20 @@
     <icon-base class="icons__image" icon-name="telegram" width="64" height="64" icon-color="#5434ad"><socialtelegram/></icon-base>
   </div>
 </template>
+<style scoped>
+.icons {
+  display: flex;
+  justify-content: flex-start;
+  width: 350px;
+  background: linear-gradient(#f7f7f7,#b7b7b7);
+}
+.icons__image {
+  padding: 7px;
+  margin: 0;
+  border: 1px solid #ccc;
+  background: linear-gradient(#f7f7f7,#b7b7b7);
+}
+</style>
 <script>
 import IconBase from './IconBase'
 import Socialfacebook from './social/facebook.vue'
